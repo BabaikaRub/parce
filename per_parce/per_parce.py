47a03ae23c1019461a68653b3c238143e349d8f3
BabaikaRub <rodionov.mihail02@mail.ru>
2022-04-08 09:48:47 +0300

    code review

--- per_parce/per_parce.py
+++ per_parce/per_parce.py
@@ -28,7 +28,7 @@ def collect_data():
         "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
         "Accept-Encoding": "gzip, deflate, br",
         "Accept-Language": "ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7",
-        "Cache-Lontrol": "max - age = 0",
+        "Cache-Control": "max - age = 0",
     }
 
     with open("report_perek.csv", 'w', encoding='cp1251') as file:
